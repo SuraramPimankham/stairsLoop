dd49bc59f74b051f812ba43df708afa564a37979
Suraram Pimankham <76487588+SuraramPimankham@users.noreply.github.com>
2023-01-21 23:14:57 +0700

    Initial commit

---
+++ stairsLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace stairsLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            for (int i = 0; i < input; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
